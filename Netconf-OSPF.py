2bed1f5ac43dc528cf50a50fe03cc41f4a80b14c
Souveiz <50000370+Souveiz@users.noreply.github.com>
2021-12-01 13:47:53 -0600

    Update Netconf-OSPF.py
    Fixed Wildmask

--- Netconf-OSPF.py
+++ Netconf-OSPF.py
@@ -23,7 +23,7 @@ n = manager.connect(
 
 
 # Attempted OSPF, it runs without error, XML shows configuration done, but does not show on the router!
-netconf_ospf = '''
+netconf_ospf1 = '''
         <config>
             <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
                 <router>
@@ -32,7 +32,25 @@ netconf_ospf = '''
                         <router-id>1.1.1.1</router-id>
                         <network>
                             <ip>10.0.0.0</ip>
-                            <mask>0.255.255.255</mask>
+                            <mask>0.0.0.255</mask>
+                            <area>0</area>
+                        </network>
+                    </ospf>
+                </router>
+            </native>
+        </config>
+'''
+
+netconf_ospf2 = '''
+        <config>
+            <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                <router>
+                    <ospf xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf">
+                        <id>1</id>
+                        <router-id>2.2.2.2</router-id>
+                        <network>
+                            <ip>10.0.0.0</ip>
+                            <mask>0.0.0.255</mask>
                             <area>0</area>
                         </network>
                     </ospf>
@@ -44,8 +62,8 @@ netconf_ospf = '''
 
 
 
-netconf_reply2 = m.edit_config(target="running", config=netconf_ospf)
+netconf_reply2 = m.edit_config(target="running", config=netconf_ospf1)
 print(xml.dom.minidom.parseString(netconf_reply2.xml).toprettyxml())
 
-netconf_reply3 = n.edit_config(target="running", config=netconf_ospf)
+netconf_reply3 = n.edit_config(target="running", config=netconf_ospf2)
 print(xml.dom.minidom.parseString(netconf_reply3.xml).toprettyxml())
